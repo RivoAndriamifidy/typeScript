a1c4954782c35ff64b84a58993aac430e29f07cc
Rivo <127342484+RivoAndriamifidy@users.noreply.github.com>
2023-12-05 10:57:59 +0300

    narrowing

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,20 @@
+"use strict";
 const compteur = document.querySelector('#compteur');
 let i = 0;
 const increment = (e) => {
+    e.preventDefault();
     i++;
-    compteur.querySelector('span').innerText = i.toString();
+    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector('span');
+    if (span) {
+        span.innerText = i.toString();
+    }
 };
-compteur.addEventListener('click', increment);
+function printId(id) {
+    if (typeof id === "number") {
+        console.log((id * 3).toString());
+    }
+    else {
+        console.log(id.toUpperCase());
+    }
+}
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener('click', increment);
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,40 @@
 const compteur = document.querySelector('#compteur')
 let i = 0
 
-const increment = (e) => {
+const increment = (e: Event) => {
+    e.preventDefault();
     i++;
-    compteur.querySelector('span').innerText= i.toString()
+    const span = compteur?.querySelector('span')
+    if (span) {
+        span.innerText= i.toString()
+    }
 }
-compteur.addEventListener('click', increment)
+
+// //Narrowing
+// function printId(id: string | number) {
+//     if (typeof id === "number") {
+//         console.log((id * 3).toString())
+//     } else {
+//         console.log(id.toUpperCase())
+//     }
+    
+// }
+
+// function exemple(a: string | string[]) {
+//     if (Array.isArray(a)) {
+//         return a[0]
+//     }
+//     return
+// }
+
+// function isDate(a: any): a is Date {
+//     return a instanceof Date    
+// }
+
+// function exemple2(a: Date | HTMLInputElement) {
+//     if (isDate(a)) {
+//         a
+//     }    
+// }
+
+compteur?.addEventListener('click', increment)
